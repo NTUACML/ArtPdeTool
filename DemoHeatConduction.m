ed176e3712148cd63a063e0f7315719510472ce3
Kavy7884 <chingkaichou@gmail.com>
2018-07-03 20:16:31 -0500

    July 3, 2018. revised domain and variables.

--- DemoHeatConduction.m
+++ DemoHeatConduction.m
@@ -1,8 +1,8 @@
-function DemoHeat
+function DemoHeatConduction
 clc; clear; close all;
 
 %% Generate domain mesh
-domain = DomainBuilder('Mesh');
+domain = DomainBuilder('Mesh', 'StraightLine');
 
 %% Generate integration rule
 integration_rule = IntegrationRule(domain);
@@ -10,20 +10,19 @@ integration_rule = IntegrationRule(domain);
 %% Generate function space
 function_space = FunctionSpace(domain);
 
-% %% Define material property
-% material = MaterialBank('Mooney');
-% 
-% %% Define variables
-% %u = Variable('displacement', 3);
-% delta_u = Variable('displacement_increment', 3);
+%% Define material property
+material = MaterialBank('Diffusivity');
+
+%% Define variables
+delta_T = Variable('Temperature_increment', 1);
 % 
 % %% Dof manager
 % dof_manager = DofManager(delta_u, function_space);
 % %global_id = dof_manager.global_id(function_space.non_zero_basis(1));
 % 
-% %% Boundary & Initial conditions
-% displacement = zeros(domain.node_number, domain.dim);
-% 
+%% Boundary & Initial conditions
+T = zeros(domain.node_number, domain.dim);
+
 % % u1 = 0 for Face 6
 % % u2 = 0 for Face 3
 % % u3 = 0 for Face 1
