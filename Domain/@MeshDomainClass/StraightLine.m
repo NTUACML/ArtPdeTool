14087bfa88b48098e5a23365f6ba2819c0fc277f
Kavy7884 <chingkaichou@gmail.com>
2018-07-15 23:17:04 -0500

    JUL 15, 2018. Domain BC finished

--- Domain/@MeshDomainClass/StraightLine.m
+++ Domain/@MeshDomainClass/StraightLine.m
@@ -14,9 +14,16 @@ function StraightLine( this )
     end
     this.boundary_connectivities_ = {1;num_element+1};
     this.boundary_element_types_ = cell(size(this.boundary_connectivities_, 1), 1);
+    this.boundary_patch_ = cell(size(this.boundary_connectivities_, 1), 2);
     for i = 1 : size(this.boundary_connectivities_, 1)
         this.boundary_element_types_{i} = ElementType.Point1;
     end
+    
+    this.boundary_patch_{1,1} = 'Front_Side'; % Name for patch
+    this.boundary_patch_{1,2} = [1]; % Boundary element  id
+    
+    this.boundary_patch_{2,1} = 'Rear_Side'; % Name for patch
+    this.boundary_patch_{2,2} = [2]; % Boundary element  id
 
     disp('Domain <Mesh type> : ');
     disp('>> generated mesh data : StraightLine!')
