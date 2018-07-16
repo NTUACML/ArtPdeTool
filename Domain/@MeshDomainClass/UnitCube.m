60ddc67ecb3512892243987f4ea9679ff46f0838
jeting <jetingwu@gmail.com>
2018-07-16 14:51:07 +0800

    domain_boundary_patch_add patch type

--- Domain/@MeshDomainClass/UnitCube.m
+++ Domain/@MeshDomainClass/UnitCube.m
@@ -14,23 +14,34 @@ function UnitCube(this)
         this.boundary_element_types_{i} = ElementType.Quad4;
     end
     % Patch definition
-    this.boundary_patch_{1,1} = 'Down_Side'; % Name for patch
-    this.boundary_patch_{1,2} = [1]; % Boundary element  id
-    
+    this.boundary_patch_{1,1} = 'Down_Side'; % Patch name
+    this.boundary_patch_{1,2} = 'boundary_element'; % Patch type
+    this.boundary_patch_{1,3} = [1]; % Patch unit id
+   
     this.boundary_patch_{2,1} = 'Up_Side';
-    this.boundary_patch_{2,2} = [2];
+    this.boundary_patch_{2,2} = 'boundary_element';
+    this.boundary_patch_{2,3} = [2];
     
     this.boundary_patch_{3,1} = 'Rear_Side';
-    this.boundary_patch_{3,2} = [3];
+    this.boundary_patch_{3,2} = 'boundary_element';
+    this.boundary_patch_{3,3} = [3];
     
     this.boundary_patch_{4,1} = 'Right_Side';
-    this.boundary_patch_{4,2} = [4];
+    this.boundary_patch_{4,2} = 'boundary_element';
+    this.boundary_patch_{4,3} = [4];
     
     this.boundary_patch_{5,1} = 'Front_Side';
-    this.boundary_patch_{5,2} = [5];
+    this.boundary_patch_{5,2} = 'boundary_element';
+    this.boundary_patch_{5,3} = [5];
     
     this.boundary_patch_{6,1} = 'Left_Side';
-    this.boundary_patch_{6,2} = [6];
+    this.boundary_patch_{6,2} = 'boundary_element';
+    this.boundary_patch_{6,3} = [6];
+    
+    this.boundary_patch_{7,1} = 'Origin';
+    this.boundary_patch_{7,2} = 'point';
+    this.boundary_patch_{7,3} = [1];
+    
     
     disp('Domain <Mesh type> : ');
     disp('>> generated mesh data : UnitCube!')
