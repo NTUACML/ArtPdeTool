151e3e56cf21ec9fb5ae4ec5f847a48c7b3a7248
Kavy7884 <chingkaichou@gmail.com>
2018-07-28 20:29:46 -0500

    Jul 27. 2018. FunctionSpace package open

---
+++ +FunctionSpace/+QuarryUnit/+FEM/QuarryUnit.m
@@ -0,0 +1,12 @@
+classdef QuarryUnit
+    %QUARRYUNIT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ +FunctionSpace/FunctionSpaceBase.m
@@ -0,0 +1,12 @@
+classdef FunctionSpaceBase
+    %FUNCTIONSPACEBASE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
