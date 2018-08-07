eb1d2362ff279c4a095645e04e1f24521e678c5f
Kavy7884 <chingkaichou@gmail.com>
2018-08-07 15:47:55 -0500

    Aug 07, 2018. PointList assigment finished.

--- +Utility/+BasicUtility/PointList.m
+++ +Utility/+BasicUtility/PointList.m
@@ -1,10 +1,30 @@
 classdef PointList < handle
-    %POINTLIST Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    %POINTLIST - A container for the point.
+    %   here is the demo code for the PointList
+    %
+    %       % Initial input matrix
+    %       input_mat = [1 2 3; 4 5 6; 7 8 9; 10 11 12; 13 14 15];
+    %
+    %       % Create PointList
+    %       point_list = PointList(input_mat);
+    %
+    %       % Modify value (only modified value in point)
+    %       point_list(1:2:5,:) = [0,0,0; 0 0 0; 0 0 0];
+    %
+    %       % Modify value (reset and new a point in point_list)
+    %       point_list{1:2} = [1, 1, 1; 2, 2, 2];
+    %
+    %       % Display all the data in PointList
+    %       disp(point_list)
+    %
+    %       % Slicing sub-matrix for the point list
+    %       mat_point = point_list(1:2:5,1);
+    %
+    %       % Slicing points cell data for the point list
+    %       cell_point = point_list{1:2:5};
     properties
-        num_rows_
-        num_cols_
+        num_rows_ = 0
+        num_cols_ = 0
         data_
     end
     
@@ -15,6 +35,17 @@ classdef PointList < handle
             this.data_ = cell(this.num_rows_, 1);
             for i = 1 : this.num_rows_
                 this.data_{i} = Point(this.num_cols_, input_mat(i,:));
+            end
+        end
+        
+        function this = subsasgn(this,S, B)
+            import Utility.BasicUtility.Point
+            if (S.type == '()')
+                this = setSliceMethod(this, S.subs, B);
+            elseif (S.type == '{}')
+                this = setCellMethod(this, S.subs, B);
+            else
+                disp('Error<PointList> ! Check your slice or cell format!');
             end
         end
         
@@ -37,9 +68,41 @@ classdef PointList < handle
         end
         
     end
-    
-    
+
     methods (Access = private)
+        function this = setSliceMethod(this, subs, B)
+            if(length(subs) ~= 2)
+                disp('Error<PointList> ! Slice quarying should be a matrix style index!');
+            end
+            [b_num_rows_, b_num_cols_] = size(B);
+            
+            subs_rows = subs{1};
+            subs_cols = subs{2};
+            if(subs_rows == ':')
+                subs_rows = 1 : this.num_rows_;
+            end
+            if(subs_cols == ':')
+                subs_cols = 1 : this.num_cols_;
+            end
+            num_run_rows = length(subs_rows);
+            num_run_cols = length(subs_cols);
+            
+            if(subs_rows(end) > this.num_rows_ ...
+                    || ...
+               subs_cols(end) > this.num_cols_)
+               disp('Error<PointList> ! Check your slice dimension for all the points!');
+            elseif(num_run_rows > b_num_rows_ ...
+                    || ...
+                   num_run_cols > b_num_cols_)
+               disp('Error<PointList> ! Check your right hand side dimension in slicing assigment!');
+            else
+                for i = 1 : num_run_rows
+                    this.data_{subs_rows(i)}.data_(subs_cols) = B(i, :);
+                end
+            end
+            
+        end
+        
         function data = getSliceMethod(this, subs)
             if(length(subs) ~= 2)
                 disp('Error<PointList> ! Slice quarying should be a matrix style index!');
@@ -87,6 +150,32 @@ classdef PointList < handle
                 data = cell(num_run_rows, 1);
                 for i = 1 : num_run_rows
                     data{i} = this.data_{subs_rows(i)};
+                end
+            end
+        end
+        
+        function this = setCellMethod(this, subs, B)
+            import Utility.BasicUtility.Point
+            if(length(subs) ~= 1)
+                disp('Error<PointList> ! Slice quarying should be a vector style index!');
+            end
+            
+            [b_num_rows_, b_num_cols_] = size(B);
+            
+            subs_rows = subs{1};
+            if(subs_rows == ':')
+                subs_rows = 1 : this.num_rows_;
+            end
+            
+            num_run_rows = length(subs_rows);
+
+            if(subs_rows(end) > this.num_rows_)
+               disp('Error<PointList> ! Check your cell dimension for all the points!');
+            elseif(num_run_rows > b_num_rows_ || b_num_cols_ > this.num_cols_)
+               disp('Error<PointList> ! Check your right hand side dimension in cell slicing assigment!');
+            else
+                for i = 1 : num_run_rows
+                    this.data_{subs_rows(i)} = Point(this.num_cols_, B(i,:));
                 end
             end
         end
--- Demo_Test.m
+++ Demo_Test.m
@@ -4,92 +4,78 @@ clc; clear; close all;
 import Domain.*
 import FunctionSpace.*
 
-%% Domain
-mesh_type_domain = DomainBuilder.create('Mesh', 'UnitCube');
-
-% Function Space
-import Utility.BasicUtility.Order
-
-% function_space = FunctionSpaceBuilder.create('FEM', mesh_type_domain); % default(Linear)
-function_space = FunctionSpaceBuilder.create('FEM', mesh_type_domain, {Order.Linear});
-
-%% Function Space (Query - Preprocess)
-import FunctionSpace.QueryUnit.FEM.QueryUnit
-import Utility.BasicUtility.Region
-import Utility.BasicUtility.Procedure
-
-fs_query_unit = QueryUnit();
-disp( '>* querying : interior domain, element - 1, parametric position is [0, 0, 0]');
-fs_query_unit.setQuery(Region.Interior, 1, [0, 0, 0]);
-[non_zeros_id, ~] = function_space.query(fs_query_unit, Procedure.Preprocess);
-disp('Non_zeros_id : ');
-disp(non_zeros_id);
-
-%% Function Space (Query - Runtime)
-import FunctionSpace.QueryUnit.FEM.QueryUnit
-import Utility.BasicUtility.Region
-import Utility.BasicUtility.Procedure
-
-fs_query_unit = QueryUnit();
-disp( '>* querying : boundary domain, element - 2, parametric position is [-1, 1, 0]')
-fs_query_unit.setQuery(Region.Boundary, 2, [-1, 1, 0]);
-[non_zeros_id, basis_value] = function_space.query(fs_query_unit); %default(Runtime)
-N = basis_value{1};
-dN_dxi = basis_value{2};
-disp('non_zeros_id :');
-disp(non_zeros_id);
-disp('N :');
-disp(N);
-disp('dN_dxi :');
-disp(dN_dxi);
-
-
-%% RKPM Domain & Function Space test
-import Utility.BasicUtility.Order
-%% Domain
-point_type_domain = DomainBuilder.create('ScatterPoint', 'StraightLine');
-% point_type_domain = DomainBuilder.create('ScatterPoint', 'UnitSquare');
-
-%% Function Space
-support_size_ratio = 2.5;
-function_space = FunctionSpaceBuilder.create('RKPM', point_type_domain, {Order.Quadratic, support_size_ratio}); % default(Linear, support_size_ratio = 1.5)
-
-%% Function Space (Query - Preprocess)
-import FunctionSpace.QueryUnit.RKPM.QueryUnitRKPM
-
-fs_query_unit = QueryUnitRKPM();
-disp( '>* querying : position is 0.06');
-fs_query_unit.setQuery(0.06);
-[non_zeros_id, basis_value] = function_space.query(fs_query_unit);
-
-N = basis_value{1};
-dN_dxi = basis_value{2};
-disp('non_zeros_id :');
-disp(non_zeros_id);
-disp('N :');
-disp(N);
-disp('dN_dxi :');
-disp(dN_dxi);
-
-% %% Test
-% import Utility.BasicUtility.*
-% % Initial input matrix
-% input_mat = [1 2 3; 4 5 6; 7 8 9; 10 11 12; 13 14 15];
-% % Create PointList
-% point_list = PointList(input_mat);
-% % Display all the data in PointList
-% disp(point_list)
-% % Slicing sub-matrix for the point list
-% mat_point = point_list(1:2:5,1);
-% % Slicing points cell data for the point list
-% cell_point = point_list{1:2:5};
+% %% Domain
+% mesh_type_domain = DomainBuilder.create('Mesh', 'UnitCube');
 % 
+% % Function Space
+% import Utility.BasicUtility.Order
+% 
+% % function_space = FunctionSpaceBuilder.create('FEM', mesh_type_domain); % default(Linear)
+% function_space = FunctionSpaceBuilder.create('FEM', mesh_type_domain, {Order.Linear});
+% 
+% %% Function Space (Query - Preprocess)
+% import FunctionSpace.QueryUnit.FEM.QueryUnit
+% import Utility.BasicUtility.Region
+% import Utility.BasicUtility.Procedure
+% 
+% fs_query_unit = QueryUnit();
+% disp( '>* querying : interior domain, element - 1, parametric position is [0, 0, 0]');
+% fs_query_unit.setQuery(Region.Interior, 1, [0, 0, 0]);
+% [non_zeros_id, ~] = function_space.query(fs_query_unit, Procedure.Preprocess);
+% disp('Non_zeros_id : ');
+% disp(non_zeros_id);
+% 
+% %% Function Space (Query - Runtime)
+% import FunctionSpace.QueryUnit.FEM.QueryUnit
+% import Utility.BasicUtility.Region
+% import Utility.BasicUtility.Procedure
+% 
+% fs_query_unit = QueryUnit();
+% disp( '>* querying : boundary domain, element - 2, parametric position is [-1, 1, 0]')
+% fs_query_unit.setQuery(Region.Boundary, 2, [-1, 1, 0]);
+% [non_zeros_id, basis_value] = function_space.query(fs_query_unit); %default(Runtime)
+% N = basis_value{1};
+% dN_dxi = basis_value{2};
+% disp('non_zeros_id :');
+% disp(non_zeros_id);
+% disp('N :');
+% disp(N);
+% disp('dN_dxi :');
+% disp(dN_dxi);
+% 
+% 
+% %% RKPM Domain & Function Space test
+% import Utility.BasicUtility.Order
+% %% Domain
+% point_type_domain = DomainBuilder.create('ScatterPoint', 'StraightLine');
+% % point_type_domain = DomainBuilder.create('ScatterPoint', 'UnitSquare');
+% 
+% %% Function Space
+% support_size_ratio = 2.5;
+% function_space = FunctionSpaceBuilder.create('RKPM', point_type_domain, {Order.Quadratic, support_size_ratio}); % default(Linear, support_size_ratio = 1.5)
+% 
+% %% Function Space (Query - Preprocess)
+% import FunctionSpace.QueryUnit.RKPM.QueryUnitRKPM
+% 
+% fs_query_unit = QueryUnitRKPM();
+% disp( '>* querying : position is 0.06');
+% fs_query_unit.setQuery(0.06);
+% [non_zeros_id, basis_value] = function_space.query(fs_query_unit);
+% 
+% N = basis_value{1};
+% dN_dxi = basis_value{2};
+% disp('non_zeros_id :');
+% disp(non_zeros_id);
+% disp('N :');
+% disp(N);
+% disp('dN_dxi :');
+% disp(dN_dxi);
 
-figure; hold on;
-x = linspace(0,1,101);
-for t = x
-    fs_query_unit.setQuery(t);
-    [non_zeros_id, basis_value] = function_space.query(fs_query_unit);
-    plot(t, basis_value{1}', 'k.');
-end
+% figure; hold on;
+% x = linspace(0,1,101);
+% for t = x
+%     fs_query_unit.setQuery(t);
+%     [non_zeros_id, basis_value] = function_space.query(fs_query_unit);
+%     plot(t, basis_value{1}', 'k.');
+% end
 
