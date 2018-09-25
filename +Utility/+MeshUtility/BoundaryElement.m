0b8e7355722794ab2886936d45c8f0f8426abd30
jeting <jetingwu@gmail.com>
2018-09-25 16:25:16 +0800

    BoundaryElement modified

--- +Utility/+MeshUtility/BoundaryElement.m
+++ +Utility/+MeshUtility/BoundaryElement.m
@@ -3,11 +3,19 @@ classdef BoundaryElement < Utility.MeshUtility.Element
     %   Detailed explanation goes here
     
     properties
+        element_id_;
+        orientation_id_;
     end
     
     methods
-        function this = BoundaryElement(dim, node_id)
+        function this = BoundaryElement(dim, node_id, element_id)
             this@Utility.MeshUtility.Element(dim, node_id);
+            this.element_id_ = element_id;
+        end
+        
+        function get_orientation(this, domain_patch)
+            element_connectivity = domain_patch.element_data_{this.element_id_}.node_id_;
+            [~, this.orientation_id_] = ismember(this.node_id_, element_connectivity);
         end
     end
     
