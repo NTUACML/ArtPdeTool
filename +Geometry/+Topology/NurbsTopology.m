2da455db70122d1d15930bd1680f5fabf3608676
jeting <jetingwu@gmail.com>
2018-10-12 12:52:20 +0800

    Nurbs Tolopogy added

---
+++ +Geometry/+Topology/NurbsTopology.m
@@ -0,0 +1,57 @@
+classdef NurbsTopology < Geometry.Topology.TopologyBase
+    %MESHTOPOLOGY Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function this = NurbsTopology(dim)
+            % import zone
+            import Utility.NurbsUtility.NurbsPatch
+            import Utility.BasicUtility.PatchType
+            import Utility.BasicUtility.Region
+            % Base class constructor
+            this@Geometry.Topology.TopologyBase(dim);
+            % New the domain patch data
+            this.domain_patch_data_ = NurbsPatch(...
+                this.dim_, 'Domain', PatchType.Nurbs, Region.Domain);
+            % New the map table for the boundary patch
+            this.boundary_patch_data_ = ...
+                containers.Map(...
+                'KeyType','char','ValueType','any');
+        end
+        
+        function domain_patch = getDomainPatch(this)
+            domain_patch = this.domain_patch_data_;
+        end
+        
+        function boundary_patch = newBoundayPatch(this, patch_name)
+            if(~isKey(this.boundary_patch_data_, patch_name))
+                import Utility.NurbsUtility.NurbsPatch
+                import Utility.BasicUtility.PatchType
+                import Utility.BasicUtility.Region
+                boundary_patch = NurbsPatch(...
+                    (this.dim_ - 1), patch_name, PatchType.Nurbs, Region.Boundary);
+                this.boundary_patch_data_(patch_name) = boundary_patch;
+            else
+                disp('Error <NurbsTopology> - newBoundayPatch!');
+                disp(['> You entered patch name: ''', patch_name, ''' existed']);
+                disp('> Please check it again.');
+            end
+        end
+        
+        function boundary_patch = getBoundayPatch(this, patch_name)
+            if(isKey(this.boundary_patch_data_, patch_name))
+                boundary_patch = this.boundary_patch_data_(patch_name);
+            else
+                boundary_patch = [];
+                disp('Error <MeshTopology> - getBoundayPatch!');
+                disp(['> You entered patch name: ''', patch_name, ''' not existed']);
+                disp('> Please new this patch via newBoundayPatch interface.');
+            end
+        end
+    end
+    
+end
+
--- +Utility/+BasicUtility/PatchType.m
+++ +Utility/+BasicUtility/PatchType.m
@@ -2,7 +2,7 @@ classdef PatchType
     %TYPE Summary of this class goes here
     %   Detailed explanation goes here
     enumeration
-        Mesh, Point
+        Mesh, Point, Nurbs
     end
 end
 
