b4de907c500c2f7d8bd3c64f9ec529cb6f7efcc3
jeting <jetingwu@gmail.com>
2018-10-18 17:25:05 +0800

    API for geometry builder from IGA tool box

--- +Demo/DemoIGADomain.m
+++ +Demo/DemoIGADomain.m
@@ -5,12 +5,19 @@ clc; clear; close all;
 
 %%  create nurbs geometry
 import Geometry.*
-geo = GeometryBuilder.create('IGA', 'Rectangle', [20, 2]);
+% geo = GeometryBuilder.create('IGA', 'Rectangle', [20, 2]);
+% nurbs_topology = geo.topology_data_{1};
+
+% object from nurbs tool box
+nurbs_cylinder = nrbcylind(3,1,[],deg2rad(0),deg2rad(360)); 
+nrbplot(nurbs_cylinder,[20,5]); 
+
+geo = GeometryBuilder.create('IGA', 'Nurbs_Object', nurbs_cylinder);
 nurbs_topology = geo.topology_data_{1};
 
-% figure; hold on;
-% nurbs_topology.domain_patch_data_.nurbs_data_.plotNurbs([20 5]);
-% hold off;
+figure; hold on;
+nurbs_topology.domain_patch_data_.nurbs_data_.plotNurbs([20 5]);
+hold off;
 
 %% Domain create
 import Domain.*
@@ -27,6 +34,9 @@ xi = {0.1 0.2};
 query_unit.query_protocol_ = {Region.Domain, xi};
 
 results = nurbs_basis.query(query_unit);
+
+
+
 
 
 end
