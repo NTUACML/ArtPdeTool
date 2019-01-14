20c6570ef9fe5d8bdd6342517b3522caaa48bfde
jeting <jetingwu@gmail.com>
2019-01-14 10:49:18 +0800

    IGA plot nurbs bug fixed

--- +Demo/DemoNewNurbsClass.m
+++ +Demo/DemoNewNurbsClass.m
@@ -9,7 +9,7 @@ import Domain.*
 %% Geometry data input
 % ArtPDE_IGA_3D_Lens_left  ArtPDE_IGA_Plane4_refined
 % ArtPDE_IGA_Plane_quarter_hole ArtPDE_IGA_Lens_bottom_left
-xml_path = './ArtPDE_IGA_Lens_bottom_left.art_geometry';
+xml_path = './ArtPDE_IGA_3D_Lens_left.art_geometry';
 
 geo = GeometryBuilder.create('IGA', 'XML', xml_path);
 nurbs_data = geo.topology_data_{1}.domain_patch_data_.nurbs_data_;
@@ -31,33 +31,33 @@ nurbs_tool = NurbsTools(nurbs_basis);
 xi = rand(5, geo_dim);
 
 % Plot nurbs
-figure; hold on; grid on; axis equal;
-nurbs_tool.plotNurbs([21 21 21]);
+figure; hold on; grid on; %axis equal;
+nurbs_tool.plotNurbs([21 21 21]); view([50 30]);
 nurbs_tool.plotControlMesh();
 hold off;
 
 % Evaluate nurbs & derivatives
-[position_org, gradient_org] = nurbs_tool.evaluateNurbs(xi, 'position', 'gradient');
+% [position_org, gradient_org] = nurbs_tool.evaluateNurbs(xi, 'position', 'gradient');
 
 %% Knot insert
-nurbs_tool.knotInsertion({[0.12 0.45 0.93] [0.28 0.84 0.55 0.6]});
+nurbs_tool.knotInsertion({[0.2 0.8] [0.3 0.7] [0.4 0.9]});
 
 nurbs_data.dispControlPoints();
 nurbs_data.dispKnotVectors();
 
 % Plot knot mesh in parametric space
-figure; hold on; axis equal;
+figure; hold on; axis equal; view([-30 30]);
 nurbs_tool.plotParametricMesh();
 hold off;
 
 % Plot nurbs
-figure; hold on; grid on; axis equal;
-nurbs_tool.plotNurbs([21 21 21]);
-nurbs_tool.plotControlMesh();
+figure; hold on; grid on; %axis equal;
+nurbs_tool.plotNurbs([15 15 15]); view([50 30]);
+% nurbs_tool.plotControlMesh();
 hold off;
 
 % Evaluate nurbs & derivatives
-[position_new, gradient_new] = nurbs_tool.evaluateNurbs(xi, 'position', 'gradient');
+% [position_new, gradient_new] = nurbs_tool.evaluateNurbs(xi, 'position', 'gradient');
 
 
 % [position] = nurbs_tool.evaluateNurbs(xi, 'position');
