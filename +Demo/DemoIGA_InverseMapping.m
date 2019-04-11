511163f33945a957e9997ab09fe162eb1bfdd449
jeting <jetingwu@gmail.com>
2019-04-11 17:58:05 +0800

    mapping merged

---
+++ +Demo/DemoIGA_InverseMapping.m
@@ -0,0 +1,58 @@
+function DemoIGA_InverseMapping
+clc; clear; close all;
+
+%% create Geometry
+import Geometry.* 
+import Domain.*
+import Utility.BasicUtility.*
+import Utility.NurbsUtility.* 
+
+xml_path = './ArtPDE_IGA_Plane_quarter_hole.art_geometry';
+
+geo = GeometryBuilder.create('IGA', 'XML', xml_path);
+nurbs_topology = geo.topology_data_{1};
+
+domain_patch = nurbs_topology.domain_patch_data_;
+
+boundary_patch_map = nurbs_topology.boundary_patch_data_;
+%% Create Domain
+iga_domain = DomainBuilder.create('IGA');
+
+%% Create Basis
+nurbs_basis = iga_domain.generateBasis(nurbs_topology);
+
+%% Create Nurbs tools
+nurbs_tool = NurbsTools(nurbs_basis);
+
+% Plot nurbs
+figure; hold on; grid on; axis equal;
+nurbs_tool.plotNurbs([21 21 21]); %view([50 30]);
+nurbs_tool.plotControlMesh();
+hold off;
+
+%% Create Mapping
+import Mapping.IGA.Mapping
+mapping = Mapping(nurbs_basis);
+patch_data = boundary_patch_map('eta_1');
+
+import BasisFunction.IGA.QueryUnit
+query_unit = QueryUnit();
+
+temp = linspace(0, 1, 17);
+
+for xi = temp
+    query_unit.query_protocol_ = {patch_data, xi, 1};
+    
+    % Evaluate physical position at xi
+    F = mapping.queryLocalMapping(query_unit);
+    physical_position = F.calPhysicalPosition();
+    
+    % Given physical coordinate, find xi
+    parametric_coordinate = mapping.inverseMapping(physical_position, patch_data);
+    
+%     disp(xi);
+%     disp(parametric_coordinate);
+end
+
+
+end
--- +Mapping/+IGA/Mapping.m
+++ +Mapping/+IGA/Mapping.m
@@ -28,6 +28,90 @@ classdef Mapping < Mapping.MappingBase
             local_pt = this.points_(non_zero_id, :);
             mapping_unit = MappingUnit(local_pt, eval_basis, query_unit.query_protocol_{1});
         end
+        
+        function parametric_coordinate = inverseMapping(this, physical_coordinate, patch_data, varargin)                           
+            import Utility.NurbsUtility.NurbsType
+            switch patch_data.nurbs_data_.type_
+                case NurbsType.Curve
+                    % Newton iteration for solving parametric coordinate
+                    % given x = f(s), for specific x, find s.
+                    
+                    if isempty(varargin)
+                        parametric_coordinate = 0; 
+                    else
+                        parametric_coordinate = varargin{1};
+                    end
+                                       
+                    iter_times = 10;
+                    [parametric_coordinate, residual, cnt] = ...
+                        this.NewtonIteration(patch_data, parametric_coordinate, physical_coordinate, iter_times);
+                     
+                    if cnt > iter_times
+                        disp('Iteration failed with the current initial guess. Activate the re-initialization process.')
+                        parametric_coordinate = this.reInitializedParametricCoordinate(physical_coordinate);
+                        
+                        [parametric_coordinate, residual, cnt] = ...
+                            this.NewtonIteration(patch_data, parametric_coordinate, physical_coordinate, iter_times);
+                    end
+                    
+                    str = ['cnt: ', num2str(cnt)];
+                    disp(str);
+                    str = ['residual: ', num2str(residual)];
+                    disp(str);
+                otherwise % Plane or Surface
+                    disp('currently nor supported...')
+            end
+        
+        end
+   
+    end
+    
+    methods (Access = private)
+        function [dC_dt, F, residual] = eval(this, patch_data, parametric_coordinate, physical_coordinate)
+            import BasisFunction.IGA.QueryUnit
+            query_unit = QueryUnit();
+            query_unit.query_protocol_ = {patch_data, parametric_coordinate, 1};
+            
+            mapping = this.queryLocalMapping(query_unit);
+            C = mapping.calPhysicalPosition();
+            dC_dt = mapping.calTangentVector();
+            
+            F = (C-physical_coordinate);
+            residual = norm(F);            
+        end
+        
+        function [parametric_coordinate, residual, cnt] = NewtonIteration(this, patch_data, parametric_coordinate, physical_coordinate, iter_times)
+            [dC_dt, F, residual] = this.eval(patch_data, parametric_coordinate, physical_coordinate);
+            
+            cnt = 0;
+            while residual > 1e-10 && cnt <= iter_times
+%                 delta_t = - residual^2 / dot(F, dC_dt);
+                delta_t = - dot(F, dC_dt) / dot(dC_dt, dC_dt);
+                
+                parametric_coordinate = parametric_coordinate + delta_t;
+                
+                if parametric_coordinate < 0
+                    parametric_coordinate = 0;
+                elseif parametric_coordinate > 1
+                    parametric_coordinate = 1;
+                end
+                
+                [dC_dt, F, residual] = this.eval(patch_data, parametric_coordinate, physical_coordinate);
+                
+                cnt = cnt + 1;
+                str = ['parametric_coordinate: ', num2str(parametric_coordinate)];
+                disp(str);
+                
+                if cnt > 5
+                    disp('here');
+                end
+            end
+        end
+        
+        function parametric_coordinate = reInitializedParametricCoordinate(this, physical_coordinate)
+            parametric_coordinate = 0;
+
+        end
     end
     
 end
